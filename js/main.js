269abcd9dc0b020c5285601c9c006beedebcf42d
domrichichi <domrichichi@gmail.com>
2016-02-29 20:44:54 -0500

    Added Verb and Story variable

--- js/main.js
+++ js/main.js
@@ -3,3 +3,9 @@ var intro = alert("Let's make a story together!");
 var colour = prompt("A colour (orange, indego, etc.}");
 
 var creature = prompt("A creature (dragon, moth, etc.");
+
+var adjective = prompt("A creature (dragon, moth, etc.");
+
+var verb = prompt("choose a past tense verb (ran, kissed etc");
+
+var story = prompt("Which story would you like? Enter the letter below: 2)snacking b)escaping c)winning")
